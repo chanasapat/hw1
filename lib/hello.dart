ef7749b6d17a68a03067d7fb77d7f316266576e9
Chanasapat Janha <chanasapat@gmail.com>
2022-08-13 21:38:46 +0700

    Initial commit

---
+++ lib/hello.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'game.dart';
+
+void main() {
+
+  var game = Game();
+  int result = 0;
+
+  print('╔══════════════════════════════════════════════');
+  print('║              GUESS THE NUMBER');
+  print('╟──────────────────────────────────────────────');
+
+  do {
+    stdout.write('║ Guess the number between 1 and 100: ');
+    var input = stdin.readLineSync();
+    var guess = int.tryParse(input!);
+
+    if (guess == null){
+      //print('Please enter number only');
+      continue;
+    }
+
+    result = game.doGuess(guess);
+  } while (result != 1);
+}
